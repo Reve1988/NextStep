47dccb9ae8748db44dc92938ba86868fa65749cd
Reve1988 <kgw1988@naver.com>
2016-10-10 20:58:14 +0900

    [NOISSUE]Remove Duplicated Test Code

--- src/com/next/step/first/CalculatorTest.java
+++ src/com/next/step/first/CalculatorTest.java
@@ -8,15 +8,15 @@ import static org.junit.Assert.assertEquals;
  * Created by giwoong.Kim on 2016. 10. 10..
  */
 public class CalculatorTest {
+    private Calculator calculator = new Calculator();
+
     @Test
     public void add() {
-        Calculator calculator = new Calculator();
         assertEquals(9, calculator.add(6, 3));
     }
 
     @Test
     public void subtract() {
-        Calculator calculator = new Calculator();
         assertEquals(3, calculator.subtract(6, 3));
     }
 }
